08b4317f5ed49508e4ace48fae585e13bc7852d4
jqc5610 <jqc5610@gmail.com>
2020-04-15 17:40:05 -0400

    create meal

--- myfitpal/db/seeds.rb
+++ myfitpal/db/seeds.rb
@@ -9,20 +9,20 @@ Workout.destroy_all
 UserWorkout.destroy_all
 UserMeal.destroy_all
 
-# User.create(name:"John",email: "mikey@gmail.com",profilepic: "pic")
+User.create(name:"John",email: "mikey@gmail.com",profilepic: "pic")
 
-# Meal.create(name:"hamburger",calories: "350", imgurl:'https://barbecuebible.com/wp-content/uploads/2013/05/featured-great-american-hamburger.jpg')
-# Meal.create(name:"chicken wrap",calories: "200", imgurl: 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/18745-italian-chicken-wraps-600x600.jpg?ext=.jpg')
-# Meal.create(name:"omlet",calories: "450", imgurl: 'https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/52/64/99/0SjpEWURRzKZt1avUq9h_omlet.jpg')
-# Meal.create(name:"bagel",calories: "270", imgurl: 'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/12/bagels.jpg')
-# Meal.create(name:"steak",calories: "600", imgurl: 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1')
-# Meal.create(name:"pasta",calories: "550", imgurl: 'https://www.gimmesomeoven.com/wp-content/uploads/2018/02/Creamy-Rose%CC%81-Pasta-with-Roasted-Tomatoes-1.jpg')
-
-
+Meal.create(name:"hamburger",calories: "350", imgurl:'https://barbecuebible.com/wp-content/uploads/2013/05/featured-great-american-hamburger.jpg')
+Meal.create(name:"chicken wrap",calories: "200", imgurl: 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/18745-italian-chicken-wraps-600x600.jpg?ext=.jpg')
+Meal.create(name:"omlet",calories: "450", imgurl: 'https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/52/64/99/0SjpEWURRzKZt1avUq9h_omlet.jpg')
+Meal.create(name:"bagel",calories: "270", imgurl: 'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/12/bagels.jpg')
+Meal.create(name:"steak",calories: "600", imgurl: 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1')
+Meal.create(name:"pasta",calories: "550", imgurl: 'https://www.gimmesomeoven.com/wp-content/uploads/2018/02/Creamy-Rose%CC%81-Pasta-with-Roasted-Tomatoes-1.jpg')
 
 
-# Workout.create(name:"running",calories: "400",imgurl:'https://www.runtastic.com/blog/wp-content/uploads/2018/04/thumbnail_8-tips-beginner_1200x800-1024x683.jpg')
-# Workout.create(name:"basketball",calories: "600",imgurl:'https://i0.wp.com/basketballsocietyonline.com/wp-content/uploads/2016/07/Obama-Basketball-Pick-up.jpg?resize=550%2C367&ssl=1')
+
+
+Workout.create(name:"running",calories: "400",imgurl:'https://www.runtastic.com/blog/wp-content/uploads/2018/04/thumbnail_8-tips-beginner_1200x800-1024x683.jpg')
+Workout.create(name:"basketball",calories: "600",imgurl:'https://i0.wp.com/basketballsocietyonline.com/wp-content/uploads/2016/07/Obama-Basketball-Pick-up.jpg?resize=550%2C367&ssl=1')
 
 
 
